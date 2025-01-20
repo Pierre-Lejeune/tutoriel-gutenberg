e7b73186d0600a7deabade8155f2fa9eb40a36af
Pierre Lejeune <hello@perlun.fr>
2025-01-20 15:36:26 +0100

    Add visible meta with register_meta

--- functions.php
+++ functions.php
@@ -12,3 +12,23 @@ function register_theme_script() {
 	wp_enqueue_script( 'script', get_template_directory_uri() . '/dist/script.js', array('jquery', 'wp-blocks', 'wp-dom'), false, true );
 }
 add_action( 'enqueue_block_editor_assets', 'register_theme_script' );
+
+
+function init_page_meta(){
+	register_meta( 'post', 'visible', array(
+		'show_in_rest' => true,
+		'object_subtype' => 'page',
+		'auth_callback' => function() {
+			return current_user_can( 'edit_posts' );
+		},
+		'type' => 'boolean',
+		'single' => true,
+		'sanitize_callback' => 'sanitize_page_visibilty'
+	) );
+}
+add_action( "init", 'init_page_meta' );
+
+
+function sanitize_page_visibility($value) {
+	return boolval($value);
+} 
